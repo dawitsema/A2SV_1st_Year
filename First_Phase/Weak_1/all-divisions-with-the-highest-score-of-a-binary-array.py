287549a3216870c769950594df8f0f0dc502ff58
dawitsema <128223243+dawitsema@users.noreply.github.com>
2023-12-20 01:22:33 +0300

    Added solution for All Divisions With the Highest Score of a Binary Array

--- First_Phase/Weak_1/all-divisions-with-the-highest-score-of-a-binary-array.py
+++ First_Phase/Weak_1/all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -4,13 +4,11 @@ class Solution:
     def maxScoreIndices(self, nums: List[int]) -> List[int]:
         n = len(nums)
 
-        # Initialize variables to keep track of counts and scores
         count_0_left = 0
         count_1_right = nums.count(1)
         max_score = 0
         max_indices = []
 
-        # Iterate through indices and calculate scores
         for i in range(n + 1):
             if i > 0:
                 count_0_left += (nums[i - 1] == 0)
@@ -18,7 +16,6 @@ class Solution:
 
             current_score = count_0_left + count_1_right
 
-            # Update maximum score and indices
             if current_score > max_score:
                 max_score = current_score
                 max_indices = [i]
