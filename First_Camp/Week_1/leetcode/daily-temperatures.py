b1e6f111a811e29d785436a1c786a4b0547e9748
dawitsema <128223243+dawitsema@users.noreply.github.com>
2024-02-23 09:43:21 +0300

    Add solution for Daily Temperatures

---
+++ First_Camp/Week_1/leetcode/daily-temperatures.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        ans = [0]*len(temperatures)
+        stack = []
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                poped = stack.pop()
+                ans[poped] = i - poped
+            stack.append(i)
+
+        return ans
+
+            
+
+
+
