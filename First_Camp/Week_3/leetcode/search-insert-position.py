68fd138f1199e3465213bc1e8446809b2619049d
dawitsema <128223243+dawitsema@users.noreply.github.com>
2024-03-06 15:45:39 +0300

    Add solution for Search Insert Position

--- First_Camp/Week_3/leetcode/search-insert-position.py
+++ First_Camp/Week_3/leetcode/search-insert-position.py
@@ -1,3 +1,13 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        return bisect_left(nums, target)
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            midd = (low + high)//2
+            if nums[midd] == target:
+                return midd
+            elif nums[midd] > target:
+                high = midd - 1
+            else:
+                low = midd + 1
+
+        return low
